44f42dcf712c252844e497f6de9b54b157b1802a
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-09-02 22:06:43 +0530

    remove * from cors origin

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async (email, title , body ) => {
         })
 
         let info = await transporter.sendMail({
-            from:  'StudyNotion || Rahul',
+            from:  'CoSkill || Rahul',
             to: `${email}`,
             subject: `${title}`,
             html: `${body}`
