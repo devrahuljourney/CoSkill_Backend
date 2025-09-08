0a3c42439c1dec1cb974ed70a187b4817edfa77c
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-09-08 23:31:01 +0530

    added redis url

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,3 +1,8 @@
-const Redis = require('ioredis');
-const redisClient = new Redis(); 
+const Redis = require("ioredis");
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+redisClient.on("connect", () => console.log("Connected to Redis"));
+redisClient.on("error", (err) => console.error("Redis Error:", err));
+
 module.exports = redisClient;
